41bf0e996505d850c51b3c333e4241610be90b1e
zcw <zcw199604@163.com>
2025-12-18 00:46:04 +0800

    first commit

---
+++ src/main/java/com/zcw/Main.java
@@ -0,0 +1,14 @@
+package com.zcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 应用启动类
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
